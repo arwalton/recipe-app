75507e717f7f578a13ee22dbd7d9c3145bc6bf5c
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-03-08 11:03:48 +0200

    Add additional unit tests to Ingredient component

--- front_end/src/components/__tests__/Ingredient.test.js
+++ front_end/src/components/__tests__/Ingredient.test.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import { render, /* screen */ } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Ingredient from '../Ingredient';
 
 it('renders without crashing', () => {
   render(<Ingredient></Ingredient>);
 });
+
+it('creates the button properly', () => {
+  const NAME = 'testName';
+  render(<Ingredient name={NAME}/>);
+  expect(screen.getByRole('button', {name: NAME})).toBeInTheDocument();
+})
+
+it('calls the onclick prop on button click', () => {
+  const MOCKFN = jest.fn();
+  render(<Ingredient onIngredientChange={MOCKFN}/>);
+  const TESTBUTTON = screen.getByRole('button');
+  fireEvent.click(TESTBUTTON);
+  expect(MOCKFN).toHaveBeenCalled();
+})
