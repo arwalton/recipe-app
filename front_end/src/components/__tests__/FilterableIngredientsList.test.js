376984b6d81593ce62818df85bdf53b074cd5a65
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-03-08 10:04:58 +0200

    Add two additional unit tests for FilterableIngredientsList

--- front_end/src/components/__tests__/FilterableIngredientsList.test.js
+++ front_end/src/components/__tests__/FilterableIngredientsList.test.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { render, /* screen */ } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FilterableIngredientsList from '../FilterableIngredientsList';
 import { BrowserRouter} from 'react-router-dom';
 
 it('renders without crashing', () => {
   render(<BrowserRouter> <FilterableIngredientsList /> </BrowserRouter>);
 });
+
+it('renders the header message', () => {
+  render(<BrowserRouter> <FilterableIngredientsList /> </BrowserRouter>);
+  expect(screen.getByText('What Do you want to cook today?')).toBeInTheDocument();
+})
+
+it('clears local storage', () => {
+  render(<BrowserRouter> <FilterableIngredientsList /> </BrowserRouter>);
+  expect(localStorage.length).toBe(0);
+})
+
